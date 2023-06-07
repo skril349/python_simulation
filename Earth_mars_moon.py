c03cef6fe2d9d014da015395b501a1a36fbe72d2
skril349 <tvives.cabaleiro@gmail.com>
2023-06-07 10:24:51 +0200

    adding subplot with circle

--- Earth_mars_moon.py
+++ Earth_mars_moon.py
@@ -36,7 +36,39 @@ def create_circle(r):
 
 radius = 5
 sphere_x_Earth, sphere_y_Earth = create_circle(radius)
-print(sphere_x_Earth.round(2))
-print("\n")
-print(sphere_y_Earth.round(2))
-print("\n")
+# print(sphere_x_Earth.round(2))
+# print("\n")
+# print(sphere_y_Earth.round(2))
+# print("\n")
+
+################# ANIMATION #######################
+frame_amount = len(t)
+width_ratio = 1.2
+y_f = -10 # [m]
+dy = 10 # [m]
+
+
+def update_plot(num):
+
+    sphere_Earth.set_data(sphere_x_Earth,sphere_y_Earth)
+
+
+    return sphere_Earth,
+
+fig = plt.figure (figsize=(16,9), dpi=120, facecolor=(0.8,0.8,0.8))
+gs = gridspec.GridSpec(3,4)
+
+ax0 = fig.add_subplot(gs[:,0], facecolor=(0.9,0.9,0.9))
+sphere_Earth, = ax0.plot ([],[],'k',linewidth=3)
+land_Earth = ax0.plot([-radius*width_ratio,radius*width_ratio],[-5,-5], linewidth=38)
+
+plt.xlim = (-radius*width_ratio, radius*width_ratio)
+plt.ylim = (y_f,y_i+dy)
+plt.xticks(np.arange(-radius, radius+1,radius))
+plt.yticks(np.arange(y_f,y_i+2*dy,dy))
+plt.ylabel("Altitude [m]")
+plt.title("Earth")
+
+
+plane_ani = animation.FuncAnimation(fig,update_plot,frames=frame_amount, interval=20, repeat=True, blit=True)
+plt.show()
