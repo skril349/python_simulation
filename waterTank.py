a28e9c7121ef7a77a17fced6e6601dbe3f0922bb
skril349 <tvives.cabaleiro@gmail.com>
2023-06-15 19:01:26 +0200

    finishing water tanks

--- waterTank.py
+++ waterTank.py
@@ -76,17 +76,27 @@ def update_plot(num):
 
     # Tank 1:
     tank_1.set_data([-radius,radius],[volume_Tank1[num],volume_Tank1[num]])
-    tank_12.set_data([0,0],[-64,volume_Tank1[num]-67])
+    tank_12.set_data([0,0],[-64,volume_Tank1[num]-64])
+    tnk_1.set_data([t[0:num], volume_Tank1[0:num]])
+    tnk_1Z.set_data([t[0:num], volume_Tank1[0:num]])
+
     # Tank 2:
     tank_2.set_data([-radius,radius],[volume_Tank2[num],volume_Tank2[num]])
-    tank_22.set_data([0,0],[-64,volume_Tank2[num]-67])
+    tank_22.set_data([0,0],[-64,volume_Tank2[num]-64])
+    tnk_2.set_data([t[0:num], volume_Tank2[0:num]])
+    tnk_2Z.set_data([t[0:num], volume_Tank2[0:num]])
+
+
 
     #Tank 3:
     tank_3.set_data([-radius,radius],[volume_Tank3[num],volume_Tank3[num]])
-    tank_32.set_data([0,0],[-64,volume_Tank3[num]-67])
+    tank_32.set_data([0,0],[-64,volume_Tank3[num]-64])
+    tnk_3.set_data([t[0:num], volume_Tank3[0:num]])
+    tnk_3Z.set_data([t[0:num], volume_Tank3[0:num]])
 
 
-    return tank_1, tank_2, tank_3, tank_12, tank_22, tank_32, 
+
+    return tank_1, tank_2, tank_3, tank_12, tank_22, tank_32, tnk_1, tnk_2, tnk_3, tnk_1Z, tnk_2Z, tnk_3Z, 
 
 fig = plt.figure(figsize=(16,9),dpi=120,facecolor=(0.8,0.8,0.8))
 gs=gridspec.GridSpec(2,3)
@@ -136,12 +146,24 @@ plt.xticks([0,22.5, 27.5,32.5,32.5+45**0.5,37.5+45**0.5,42.5+45**0.5, 60])
 plt.yticks(np.arange(volume_i,volume_f+dVol,dVol))
 plt.ylabel('tank volume [m^3]')
 plt.xlabel("Time [s]")
-plt.title('Tank 3')
+plt.title('Tanks')
 plt.legend(loc='upper right',fontsize='small')
 plt.grid(True)
 ############ subplot 4 ##########################
 
 ax4 = fig.add_subplot(gs[1,2],facecolor=(0.9,0.9,0.9))
+tnk_1Z, =ax4.plot([],[],'blue',linewidth=3,label="Tank 1")
+tnk_2Z, =ax4.plot([],[],'green',linewidth=3,label="Tank 2")
+tnk_3Z, =ax4.plot([],[],'red',linewidth=3,label="Tank 3")
+plt.xlim(50,t_end)
+plt.ylim(volume_i, volume_f)
+plt.xticks([0,22.5, 27.5,32.5,32.5+45**0.5,37.5+45**0.5,42.5+45**0.5, 60])
+plt.yticks(np.arange(volume_i,volume_f+dVol,dVol))
+plt.ylabel('tank volumes zoomed [m^3]')
+plt.xlabel("Time [s]")
+plt.legend(loc='upper right',fontsize='small')
+plt.axis([38,50,47,53])
+plt.grid(True)
 
 plane_ani = animation.FuncAnimation(fig,update_plot,frames=frame_amount, interval=20, repeat=True, blit=True)
 plt.show()
